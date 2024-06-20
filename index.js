0e4d9755caa71f3449a78ee776f91d38d61fd962
anubhab-nath098 <anubhabnath19@gmail.com>
2024-06-20 19:43:52 +0530

    changes made in cors

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const server = app.listen(process.env.PORT,()=>{
 const io=socket(server,{
     cors:{
         origin:allowedOrigins,
-        Credentials:true,
+        credentials:true,
     }
 })
 
